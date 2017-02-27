89d5f489e2dd8479fa236c6227bbcfd69b26e7bf
jianpeng.zhang <msmfzjp@outlook.com>
2017-02-27 21:16:36 +0800

    Spring练习例子

---
+++ src/com/spring_test/Bean作用域/App.java
@@ -0,0 +1,29 @@
+package com.spring_test.Bean作用域;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring_test.bean.CustomerService;
+
+import java.nio.file.Paths;
+
+/**
+ * @author jianpeng.zhang
+ * @since 2017/2/27.
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        String path = Paths.get(App.class.getPackage().getName().replace(".", "/"), "Spring-Customer.xml").toString();
+        ApplicationContext context = new ClassPathXmlApplicationContext(path);
+
+        CustomerService custA = (CustomerService)context.getBean("customerService");
+        custA.setMessage("Message by custA");
+        System.out.println("Message : " + custA.getMessage());
+
+        //retrieve it again
+        CustomerService custB = (CustomerService)context.getBean("customerService");
+        System.out.println("Message : " + custB.getMessage());
+    }
+}
---
+++ src/com/spring_test/集合的Factory/App.java
@@ -0,0 +1,23 @@
+package com.spring_test.集合的Factory;
+
+import com.spring_test.bean.CustomerCollection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.nio.file.Paths;
+
+/**
+ * @author jianpeng.zhang
+ * @since 2017/2/27.
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        String path =
+                Paths.get(App.class.getPackage().getName().replace(".", "/"), "applicationContext.xml").toString();
+        ApplicationContext context = new ClassPathXmlApplicationContext(path);
+        CustomerCollection cust = (CustomerCollection) context.getBean("CustomerBean");
+        System.out.println(cust);
+    }
+}
